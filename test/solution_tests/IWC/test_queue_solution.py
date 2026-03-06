2aca3d86408e3524e2950cb72ab7813cd670d247
sbx_user1051 <sbx_user1051@169.254.44.181>
2026-03-06 14:22:18 +0000

    Fri Mar 06 14:22:18 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -17,15 +17,29 @@ def test_rule_of_three() -> None:
     run_queue(
         [
             call_enqueue(user_id=1, provider="companies_house", timestamp="2025-10-20 12:00:00").expect(1),
-            call_enqueue(user_id=2, provider="bank_statements", timestamp="2025-10-20 12:00:00").expect(1),
-            call_enqueue(user_id=1, provider="id_verification", timestamp="2025-10-20 12:00:00").expect(1),
-            call_enqueue(user_id=1, provider="bank_statements", timestamp="2025-10-20 12:00:00").expect(1),
+            call_enqueue(user_id=2, provider="bank_statements", timestamp="2025-10-20 12:00:00").expect(2),
+            call_enqueue(user_id=1, provider="id_verification", timestamp="2025-10-20 12:00:00").expect(3),
+            call_enqueue(user_id=1, provider="bank_statements", timestamp="2025-10-20 12:00:00").expect(4),
+            call_size().expect(4),
             call_dequeue().expect("companies_house", 1),
             call_dequeue().expect("id_verification", 1),
             call_dequeue().expect("bank_statements", 1),
             call_dequeue().expect("bank_statements", 2),
         ]
     )
+
+
+def test_timestamp_ordering() -> None:
+    run_queue(
+        [
+            call_enqueue(user_id=1, provider="bank_statements", timestamp="2025-10-20 12:00:00").expect(1),
+            call_enqueue(user_id=2, provider="bank_statements", timestamp="2025-10-20 12:05:00").expect(2),
+            call_size().expect(2),
+            # call_dequeue().expect("bank_statements", 2),
+            # call_dequeue().expect("bank_statements", 1),
+        ]
+    )
+
 
 
 
