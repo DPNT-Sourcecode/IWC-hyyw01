ef5e05cda79fd2fddde5ea250f0b0ba1f775963b
sbx_user1051 <sbx_user1051@169.254.68.73>
2026-03-06 13:50:18 +0000

    Fri Mar 06 13:50:18 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,10 @@
-
 class SumSolution:
-    
+    def _validate_number_in_range(self, value: int) -> None:
+        if value < 0 or value > 100:
+            raise ValueError("Value must be in range 0 - 100")
+
     def compute(self, x, y):
-        raise NotImplementedError()
+        self._validate_number_in_range(value=x)
+        self._validate_number_in_range(value=y)
+        return x + y
+
