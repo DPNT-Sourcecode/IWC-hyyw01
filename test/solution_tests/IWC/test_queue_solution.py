1a67504f6b2dc8a255d431d750f18b7e3d9127d5
sbx_user1051 <sbx_user1051@169.254.44.181>
2026-03-06 14:16:18 +0000

    Fri Mar 06 14:16:18 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -4,17 +4,30 @@ from .utils import call_dequeue, call_enqueue, call_size, iso_ts, run_queue
 
 
 def test_enqueue_size_dequeue_flow() -> None:
-    run_queue([
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-        call_size().expect(1),
-        call_dequeue().expect("companies_house", 1),
-    ])
+    run_queue(
+        [
+            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+            call_size().expect(1),
+            call_dequeue().expect("companies_house", 1),
+        ]
+    )
 
 
 def test_rule_of_three() -> None:
+    call_enqueue(user_id=1, provider="companies_house", timestamp="2025-10-20 12:00:00")
+    call_enqueue(user_id=2, provider="bank_statements", timestamp="2025-10-20 12:00:00")
+    call_enqueue(user_id=1, provider="id_verification", timestamp="2025-10-20 12:00:00")
+    call_enqueue(user_id=1, provider="bank_statements", timestamp="2025-10-20 12:00:00")
+    assert call_dequeue().expect("companies_house", 1)
+    assert call_dequeue().expect("id_verification", 1)
+    assert call_dequeue().expect("bank_statements", 1)
+    assert call_dequeue().expect("bank_statements", 2)
 
-call_enqueue(user_id=1, provider="companies_house",   timestamp='2025-10-20 12:00:00') 
-call_enqueue(user_id=2, provider="bank_statements",   timestamp='2025-10-20 12:00:00')
-call_enqueue(user_id=1, provider="id_verification",   timestamp='2025-10-20 12:00:00')
-call_enqueue(user_id=1, provider="bank_statements",   timestamp='2025-10-20 12:00:00')
+
+def test_timestamp_ordering() -> None:
+    call_enqueue(user_id=1, provider="bank_statements", timestamp="2025-10-20 12:05:00")
+    call_enqueue(user_id=2, provider="bank_statements", timestamp="2025-10-20 12:00:00")
+    call_dequeue().expect("bank_statements", 1)
+    call_dequeue().expect("bank_statements", 2)
+
 
